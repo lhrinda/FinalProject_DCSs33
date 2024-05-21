f53766f1cfdeea75e2724c5e7af64efbfdf13290
Fran Miele <162506996+BlueBoi26@users.noreply.github.com>
2024-05-21 12:35:03 -0400

    added banner to main

--- final-project/src/Pages/movies.jsx
+++ final-project/src/Pages/movies.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import MoviePoster from '../MoviePoster';
 
 function Movies() {
-  return (
-    <div>
-      <div className="h-14 bg-gradient-to-r from-cyan-500 to-blue-500">
-        <h1 className="text-center text-3xl font-bold mb-4 text-white">Liked Movies</h1>
-      </div>
+  return(
+    <div class="h-14 bg-gradient-to-r from-cyan-500 to-blue-500"><h1 className="text-center text-3xl font-bold mb-4 text-white">Liked Movies</h1>
     </div>
   );
 }
+
 
 export default Movies;
