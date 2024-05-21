683e5623fee4b208b81b323e84a2d7e49aa7790b
Fran Miele <162506996+BlueBoi26@users.noreply.github.com>
2024-05-21 17:33:53 -0400

    Fixed the homepage, banner is bugged

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -11,12 +11,24 @@ function App() {
   const [likedMovies, setLikedMovies] = useState([]);
   const [wishlistMovies, setWishlistMovies] = useState([]);
 
+  const handleLike = (movie) => {
+    setLikedMovies((prev) => [...prev, movie]);
+    /*Learned about array fromhttps://www.w3schools.com/js/js_arrays.asp#gsc.tab=0*/
+  };
 
+  const handleWishlist = (movie) => {
+    setWishlistMovies((prev) => [...prev, movie]);
+  };
 
   const renderPage = () => {
     switch (currentPage) {
       case 'Movies':
-        return (<Movies />);
+        return (
+          <div className="flex flex-wrap">
+            <MoviePoster name="Movie 1" image="https://via.placeholder.com/200x300" onLike={handleLike} onWishlist={handleWishlist} />
+            <MoviePoster name="Movie 2" image="https://via.placeholder.com/200x300" onLike={handleLike} onWishlist={handleWishlist} />
+          </div>
+        );
       case 'Liked':
         /*Learned about switching states from https://www.w3schools.com/js/js_switch.asp*/
         return <Liked movies={likedMovies} />;
